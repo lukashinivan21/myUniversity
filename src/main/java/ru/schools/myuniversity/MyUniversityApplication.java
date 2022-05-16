9d30efaa97b07ee272ca503dcbb2eccc08d3f754
Ivan <uvanes21@yandex.ru>
2022-05-16 18:37:18 +0300

    First commit

---
+++ src/main/java/ru/schools/myuniversity/MyUniversityApplication.java
@@ -0,0 +1,13 @@
+package ru.schools.myuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/schools/myuniversity/MyUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.schools.myuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
