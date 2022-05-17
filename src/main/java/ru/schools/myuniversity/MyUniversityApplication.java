9b9f18c515f0cd13b3e5672d3fc47dd002da2876
Ivan <uvanes21@yandex.ru>
2022-05-17 07:55:12 +0300

    Work1

--- src/main/java/ru/schools/myuniversity/MyUniversityApplication.java
+++ src/main/java/ru/schools/myuniversity/MyUniversityApplication.java
@@ -1,9 +1,11 @@
 package ru.schools.myuniversity;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class MyUniversityApplication {
 
     public static void main(String[] args) {
