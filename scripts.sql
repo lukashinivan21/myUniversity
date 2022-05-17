ffe98b37d38949b6147514ab3702a878874c2e30
Ivan <uvanes21@yandex.ru>
2022-05-17 08:37:33 +0300

    Work2

---
+++ scripts.sql
@@ -0,0 +1,9 @@
+select * FROM student where age > 24 and age < 29;
+
+select student.name FROM student;
+
+select * FROM student where name like '%e%';
+
+select * FROM student where age < id;
+
+select * FROM student where age > 26 ORDER BY age DESC;
